613f0301be38afb202b09cb6eba18362538acc42
Ross Kelly <rsjkd12@gmail.com>
2019-02-09 11:45:32 +0000

    Large commit to add the mysql container and connect to it in the app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,31 +1,68 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+import java.sql.*;
+import com.mysql.jdbc.Driver;
 
 public class App {
 
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "RDJK")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2018/19")
-                .append("result", new Document("CW", 3).append("EX", 1));
-        // Add document to collection
-        collection.insertOne(doc);
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Driver d;
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver.");
+            System.exit(-1);
+        }
+
+        //Connection to the database
+        Connection con = null;
+        int retries = 100;
+
+        for(int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to Database...");
+            try
+            {
+                //Wait a bit for db to start
+                Thread.sleep(30000);
+
+                //Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+
+                //Wait a bit
+                Thread.sleep(10000);
+
+                //Exit loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread Interrupted? Should not happen...");
+            }
+        }
+
+        if (con != null)
+        {
+            try
+            {
+                //close the connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
 
         System.out.println("Boo yah!");
     }
