71bd69ae2c460c7d9246d6f5f0ed302f076c0704
Ross Kelly <rsjkd12@gmail.com>
2019-03-19 13:31:19 +0000

    Example int test

--- src/test/java/com/napier/sem/SEMIntTests.java
+++ src/test/java/com/napier/sem/SEMIntTests.java
@@ -1,4 +1,13 @@
 package com.napier.sem;
 
-public class SEMIntTests {
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SEMIntTests {
+
+    @Test
+    void ExampleIntTest(){
+        assertEquals(34, 34);
+    }
+
 }
